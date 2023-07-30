e81b595e833e556dd5b983f16d290715624b6216
riadhBaklouti <bakloutir2@gmail.com>
2023-07-30 10:44:49 +0200

    feat(dashboard): styling the dashboard content

--- src/app/feature/private/dashboard/components/dashboard-main/dashboard.component.ts
+++ src/app/feature/private/dashboard/components/dashboard-main/dashboard.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
   constructor() { }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { LanguageLevelItemComponent } from './language-level-item/language-level-item.component';
 import { ReminderItemComponent } from './reminder-item/reminder-item.component';
+import {NgClass} from "@angular/common";
 
 
 
 @NgModule({
-  declarations: [
-    ProgressBarComponent,
-    LanguageLevelItemComponent,
-    ReminderItemComponent
-  ],
-  exports: [
-    ProgressBarComponent,
-    LanguageLevelItemComponent,
-    ReminderItemComponent
-  ]
+    declarations: [
+        ProgressBarComponent,
+        LanguageLevelItemComponent,
+        ReminderItemComponent
+    ],
+    imports: [
+        NgClass
+    ],
+    exports: [
+        ProgressBarComponent,
+        LanguageLevelItemComponent,
+        ReminderItemComponent
+    ]
 })
 export class SharedModule { }
