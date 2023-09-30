5e7c5e21e3c45cca76654d16648b6bd6417cf7e3
riadhBaklouti <bakloutir2@gmail.com>
2023-09-30 16:15:57 +0200

    feat(dashboard): finishing styling the dashboard

---
+++ src/app/feature/private/learning-plan/components/learning-plan/learning-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-plan',
+  templateUrl: './learning-plan.component.html',
+  styleUrls: ['./learning-plan.component.scss']
+})
+export class LearningPlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/language-level-item/language-level-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-language-level-item',
+  templateUrl: './language-level-item.component.html',
+  styleUrls: ['./language-level-item.component.scss']
+})
+export class LanguageLevelItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
